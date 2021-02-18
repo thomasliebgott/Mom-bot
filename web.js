d0bd8fd55cdfd5c370232b1464647bc4d1e6af02
thomas liebgott <thomas.liebgott@groupe-esigelec.org>
2021-02-18 11:50:20 +0100

    3rd change

--- web.js
+++ web.js
@@ -42,7 +42,7 @@ app.get('/bornes', (req,res) => {
 	db.connect(function(err) {
 		if (err) throw err;
 		console.log("Connecté à la base de données MySQL!");
-		db.query("SELECT idBorne, Lieu FROM bornes", function (err, result) {
+		db.query("SELECT idBorne, Lieu FROM Bornes", function (err, result) {
 			console.log(result);
 			res.status(200).json(result);
 		});
