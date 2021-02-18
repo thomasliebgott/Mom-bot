a9447955eccefe6970087771cb5904ed6e502e57
thomas liebgott <thomas.liebgott@groupe-esigelec.org>
2021-02-18 11:44:02 +0100

    2nd change

--- web.js
+++ web.js
@@ -15,12 +15,58 @@ app
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
   
 //1. Hello world API des users
- app.get('/users', (req,res) => {
-     res.send("Liste des users")
- })
+// app.get('/users', (req,res) => {
+    // res.send("Liste des users")
+// })
 
 //2. API static
 // app.get('/users', (req,res) => {
     // res.status(200).json(users)
 // })
+
+//3. API static by id
+app.get('/users/:id', (req,res) => {
+    const id = parseInt(req.params.id)
+    const user = users.find(u => u.id === id)
+    res.status(200).json(user)
+})
+
+//4. Dynamic API users
+app.get('/bornes', (req,res) => {
+	const db = mysql.createConnection({
+	   host: "sql2.freemysqlhosting.net",
+	   user: "sql2393753",
+	   password: "nT2*qW6!",
+	   database: 'sql2393753'
+	});
+	db.connect(function(err) {
+		if (err) throw err;
+		console.log("Connecté à la base de données MySQL!");
+		db.query("SELECT idBorne, Lieu FROM bornes", function (err, result) {
+			console.log(result);
+			res.status(200).json(result);
+		});
+	});
+})
+
+//5. Update user
+app.put('/users/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+	const user = req.body;
+	console.log(id);
+	console.log(req);
+	const db = mysql.createConnection({
+	   host: "sql2.freemysqlhosting.net",
+	   user: "sql2392547",
+	   password: "zW5%aJ9*",
+	   database: 'sql2392547'
+	});
+	db.connect(function(err) {
+		if (err) throw err;
+		db.query("UPDATE users SET lastName = '" + user.lastName + "', firstName = '" + user.firstName + "' WHERE id = " + id, function (err, result) {
+		});
+	});
+    res.status(200).json(user)
+})
+
 
