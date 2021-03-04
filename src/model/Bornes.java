2627771d84adba8c078519bd7fc0005f98f3953e
thomasliebgott <thomas.liebgott@live.fr>
2021-03-04 09:57:18 +0100

    ajoutClassesIHM

--- src/model/Bornes.java
+++ src/model/Bornes.java
@@ -7,5 +7,11 @@ import java.util.ArrayList;
 
 
 public class Bornes {
+	
+
+	  public static void main(String[] args)
+	  {
 	System.out.println("Cet identifiant de fournisseur existe d�j�. Ajout impossible !"); 
+	  }
+	  
 }
