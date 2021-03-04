251eb7e0d44ae3df886bc93b98fca16df42a390e
thomasliebgott <thomas.liebgott@live.fr>
2021-03-04 10:08:09 +0100

    re IHM

--- src/gui/ChoixService.java
+++ src/gui/ChoixService.java
@@ -1,5 +1,43 @@
 package gui;
 
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
 public class ChoixService {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ChoixService window = new ChoixService();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public ChoixService() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 
 }
