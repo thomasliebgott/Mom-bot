377ff579093d3d57833b4ee0736be0474d6b622b
thomasliebgott <thomas.liebgott@live.fr>
2021-03-04 09:44:27 +0100

    test

--- src/model/Bornes.java
+++ src/model/Bornes.java
@@ -1,5 +1,11 @@
 package model;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
 
 public class Bornes {
-
+	System.out.println("Cet identifiant de fournisseur existe d�j�. Ajout impossible !"); 
 }
